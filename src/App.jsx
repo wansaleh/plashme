4f5c9aa35cd8d5c138111a3bce55595a00103e35
Wan Saleh <wansaleh@gmail.com>
2021-04-24 19:02:49 +0800

    Update.

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ function App() {
   }, 1000);
 
   return (
-    <div className="absolute inset-0">
+    <div className="absolute inset-0 flex items-center justify-center">
       <div
-        className="text-[80px] font-bold absolute bottom-0 left-0 m-16 leading-none text-white"
+        className="text-[200px] font-thin tracking-widest m-16 leading-none text-white"
         style={{ textShadow: '0 0 10px rgba(0,0,0,0.35)' }}
       >
-        {format(date, 'hh:mm:ss a')}
+        {format(date, 'hh:mm a')}
       </div>
     </div>
   );
