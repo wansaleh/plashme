2d8148620db0b2c6fae6b33353269942a4cb03f4
Wan Saleh <wansaleh@gmail.com>
2021-04-25 05:08:00 +0800

    Update.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
-// import macrosPlugin from 'vite-plugin-babel-macros';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-  // esbuild: {
-  //   jsxFactory: 'jsx',
-  //   jsxInject: `import {jsx, css} from '@emotion/react'`
-  // }
+  plugins: [reactRefresh()],
+  esbuild: {
+    jsxFactory: 'jsx',
+    jsxInject: `import { jsx, css } from '@emotion/react'`
+  }
 });
