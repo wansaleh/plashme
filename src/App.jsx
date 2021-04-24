fb2a7159d582f23d19476a3ee3abc6a65424ee58
Wan Saleh <wansaleh@gmail.com>
2021-04-25 00:54:48 +0800

    Update.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { format } from 'date-fns';
 import React, { useState } from 'react';
 import { useInterval } from 'react-use';
 
-function App() {
+export default function App() {
   const [date, setDate] = useState(new Date());
 
   useInterval(() => {
@@ -12,15 +12,11 @@ function App() {
   return (
     <div className="absolute inset-0 flex items-center justify-center leading-none text-white">
       <div style={{ textShadow: '0 0 40px rgba(0,0,0,0.5)' }}>
-        <div className="text-[200px] font-bold tracking-wider">
-          {format(date, 'hh:mm a')}
-        </div>
-        <div className="mr-4 -mt-0 text-6xl text-right">
+        <div className="text-[12vw] font-bold">{format(date, 'hh:mm a')}</div>
+        <div className="mr-4 -mt-0 text-[3vw] text-right">
           {format(date, 'EEEE, LLLL d')}
         </div>
       </div>
     </div>
   );
 }
-
-export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
+// import macrosPlugin from 'vite-plugin-babel-macros';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()]
+  // esbuild: {
+  //   jsxFactory: 'jsx',
+  //   jsxInject: `import {jsx, css} from '@emotion/react'`
+  // }
 });
