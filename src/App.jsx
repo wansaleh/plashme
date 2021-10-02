6ca117478199aa401feb11d91d3103dc5d06fdfa
Wan Saleh <wansaleh@gmail.com>
2021-10-02 10:31:21 +0800

    Update.

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,11 @@ export default function App() {
   return (
     <div className="flex absolute inset-0 justify-center items-center leading-none text-white">
       <div style={{ textShadow: '0 0 40px rgba(0,0,0,0.5)' }}>
-        <div className="text-[18vw] font-bold">{format(date, 'hh:mm a')}</div>
+        <div className="text-[16vw] font-bold tracking-tighter">
+          {format(date, 'hh:mm a')}
+        </div>
         <div
-          className="mr-4 -mt-0 text-[6vw] text-right"
+          className="mr-4 -mt-0 text-[4vw] text-right"
           style={{
             textShadow: '0 0 20px rgba(0,0,0,0.5), 0 0 2px rgba(0,0,0,0.2)'
           }}
